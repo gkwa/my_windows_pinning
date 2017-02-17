7a4c9f4aec6093fdb30f75edd743b8f593c19670
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-17 15:19:55 -0800

    Optimize: don't call PinTo10v2 if OS doesn't support it

--- update_icons_taskbar.ps1
+++ update_icons_taskbar.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 1.5
+.VERSION 1.6
 
 .GUID 7891e8ce-78f1-4b38-adcb-e6a43cc6d3b9
 
@@ -58,6 +58,13 @@ copy //tsclient/tmp/doit.ps1 .; . .\doit.ps1
 
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1" -Force
 
+function pinto10{
+	if($pinto10_ok){
+		PinTo10v2 $args
+	}
+}
+
+
 if(!(Get-Command PinTo10v2 -ea 0)) { 
     if(!(test-path "${env:ProgramFiles}\PinTo10v2\bin")){
         mkdir -force "${env:ProgramFiles}\PinTo10v2\bin" >$null
@@ -76,9 +83,11 @@ if($TBDIR -eq $null){
 }
 
 # Check if PinTo10v2 runs on this machine
+$pinto10_ok = $true
 $output = PinTo10v2 /pintb C:\Windows\system32\cmd.exe
 if($output -like 'I only work on windows 7 & 10 - Exiting...')
 {
+	$pinto10_ok = $false
 	Write-Warning "$output"
 }
 
@@ -87,7 +96,7 @@ if($output -like 'I only work on windows 7 & 10 - Exiting...')
 # Just remove from taskbar
 
 # Windows Media Player
-PinTo10v2 /unpintb "${env:SYSTEMDRIVE}\Program*\Windows Media Player\wmplayer.exe" | Out-Null
+pinto10 /unpintb "${env:SYSTEMDRIVE}\Program*\Windows Media Player\wmplayer.exe"
 
 # Add to taskbar
 
@@ -245,7 +254,7 @@ foreach($h in $list) {
 
     # unpin first so we can run muliple times without creating
     # duplicates
-    PinTo10v2 /unpintb $h.get_item("ShortcutFilePath") | Out-Null
+    pinto10 /unpintb $h.get_item("ShortcutFilePath")
 }
 
 foreach($h in $list) {
@@ -280,5 +289,5 @@ foreach($h in $list) {
       -WorkingDirectory $h.get_item("WorkingDirectory") `
       -PinToTaskbar
 
-    PinTo10v2 /pintb $h.get_item("ShortcutFilePath") | Out-Null
+    pinto10 /pintb $h.get_item("ShortcutFilePath")
 }
